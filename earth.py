a7eb6c970462e49c520856a3da4b4c9536edf0f0
SantiagoKiselicki <smkiselicki@mail.austral.edu.ar>
2025-04-15 19:26:01 +0000

    fix tests

--- earth.py
+++ earth.py
@@ -2,6 +2,6 @@ def earth():
     x = "Bangladesh"
     y = "Barbados"
     
-    print("The result of", x, "comes first in the dictionary than", y, "is", x < y)
-    print("The result of", y, "comes first in the dictionary than", x, "is", y < x)
+    print("The result of", x, "comes first in the dictionary than", y, "is", f"{x < y}.")
+    print("The result of", y, "comes first in the dictionary than", x, "is", f"{y < x}.")
 
