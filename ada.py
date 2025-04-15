9b628422af32a017ec71f1ad497a296d3ed1772f
SantiagoKiselicki <smkiselicki@mail.austral.edu.ar>
2025-04-15 16:19:30 -0300

    exercise done

--- ada.py
+++ ada.py
@@ -1,3 +1,9 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+
+    # Formatos solicitados
+    print(first_name.lower(), last_name.lower())     # ada lovelace
+    print(first_name.title(), last_name.title())     # Ada Lovelace
+    print(first_name.upper(), last_name.upper())     # ADA LOVELACE
+    print("\t" + first_name.lower(), last_name.lower())  #     ada lovelace
--- change.py
+++ change.py
@@ -1,3 +1,22 @@
 def change():
     expense = 23.75
     money = 100
+
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print()  # línea en blanco
+
+    # Cálculo del vuelto
+    change = money - expense
+
+    # Extraemos pesos y centavos
+    pesos = int(change)
+    centavos = int(round((change - pesos) * 100))
+
+    print("Vuelto\n")
+    print("Pesos:")
+    print(pesos)
+    print("Centavos:")
+    print(centavos)
